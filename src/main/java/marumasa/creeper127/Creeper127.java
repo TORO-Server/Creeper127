892ef0211b6d24c0fe67c82b859b40abe04162be
malken21 <Malken_Marumasa@outlook.jp>
2023-09-06 19:12:26 +0900

    setup

---
+++ src/main/java/marumasa/creeper127/Creeper127.java
@@ -0,0 +1,9 @@
+package marumasa.creeper127;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Creeper127 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/marumasa/creeper127/client/Creeper127Client.java
@@ -0,0 +1,10 @@
+package marumasa.creeper127.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Creeper127Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
